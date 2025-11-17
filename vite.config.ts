b5e9ed069d92a296b794ff133e79f16b378cb219
Al1ceG <94125456+Al1ceG@users.noreply.github.com>
2025-11-17 09:25:57 +0000

    try open:True

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    open: true
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
